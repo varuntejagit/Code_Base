24d859ce3cea4e91a1b6863cf20ea78f9244f44c
varuntejagit <varuntejagit>
2024-08-21 17:35:13 +0000

    delta file updated

--- Notebooks_git/Delta/Delta.py
+++ Notebooks_git/Delta/Delta.py
@@ -17,7 +17,30 @@
 # MAGIC
 # MAGIC
 # MAGIC %sql
-# MAGIC -- 
+# MAGIC -- it will say history of operations done over a table or data 
 # MAGIC describe history lakehouse_dev.default.test_table
 # MAGIC
 # MAGIC
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC -- when we perform action like insert a new paruet file is created and if we 
+# MAGIC insert into  lakehouse_dev.default.test_table values (1,"John",30,'New York','NY',100000.0)
+# MAGIC
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from lakehouse_dev.default.test_table
+
+# COMMAND ----------
+
+display(spark.read.parquet("s3://varun-databricks-s3-bucket/parquet_file_logs_view/part-00000-23606455-aa8f-4746-b018-6abb248456ee-c000.snappy.parquet"))
+# it is used to read data in the paruet file created for that we need to paste the paruet file in the some other file in storage it is spark constrait 
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC insert into  lakehouse_dev.default.test_table values (2,"Varun",24,'Allagadda','AP',500000.0)
